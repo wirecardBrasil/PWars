3975d0b74a1bf1119d0055570f05f6880a929586
Caio Incau <caioincau@gmail.com>
2017-08-04 10:37:23 -0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,258 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    visibleCards: {},
+    selectedItens: [],
+    previous: '',
+    next: ''
+    // spinner: document.querySelector('.loader'),
+    // cardTemplate: document.querySelector('.cardTemplate'),
+    // container: document.querySelector('.main'),
+    // addDialog: document.querySelector('.dialog-container')
+  };
+  var showResults = () => {
+    var container = document.querySelector(".pwa-container");
+    var content = '<div class="result-list">'
+    app.selectedItens.forEach((e) => {
+      content += generateCardForCharacter(e)
+    })
+    content += '</div>'
+    container.innerHTML = content
+    // window.localStorage.lastResults = JSON.stringify(app.selectedItens)
+  }
+
+
+  var generateCardForCharacter = (char) => {
+    return `<div class="result-list__card demo-card-square mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title mdl-card--expand">
+        <h2 class="mdl-card__title-text">${char.name}</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
+        <p> <span class="result-list__card-label"> Altura: </span> ${char.height} cm </p>
+        <p> <span class="result-list__card-label"> Peso: </span> ${char.mass} kilos</p>
+        <p> <span class="result-list__card-label"> Gênero: </span> ${char.gender} </p>
+        <p> <span class="result-list__card-label"> Nascimento: </span> ${char.birth_year} </p>
+      </div>
+    </div>`
+  }
+  fetch('https://swapi.co/api/people/?format=json')
+    .then(function(response) {
+      let json = response.json()
+      .then(json => {
+        app.selectedItens = json.results
+        app.previous = json.previous
+        app.next = json.next
+        showResults()
+      })
+    }).catch((error) => {
+      console.log(error)
+      getFromCache()
+      // showResults()
+    })
+
+
+
+
+
+  var getFromCache = () => {
+    caches.match('https://swapi.co/api/people/?format=json')
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json.results)
+      app.selectedItens = json.results
+      showResults()
+    })
+  }
+    
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  // document.getElementById('butAddCancel').addEventListener('click', function() {
+  //   // Close the add new city dialog
+  //   app.toggleAddDialog(false);
+  // });
+
+
+  /*****************************************************************************
+   *
+   * Methods to update/refresh the UI
+   *
+   ****************************************************************************/
+
+  
+
+
+  /*****************************************************************************
+   *
+   * Methods for dealing with the model
+   *
+   ****************************************************************************/
+
+  /*
+   * Gets a forecast for a specific city and updates the card with the data.
+   * getForecast() first checks if the weather data is in the cache. If so,
+   * then it gets that data and populates the card with the cached data.
+   * Then, getForecast() goes to the network for fresh data. If the network
+   * request goes through, then the card gets updated a second time with the
+   * freshest data.
+   */
+  // app.getForecast = function(key, label) {
+  //   var statement = 'select * from weather.forecast where woeid=' + key;
+  //   var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
+  //       statement;
+  //   // TODO add cache logic here
+  //   if ('caches' in window) {
+  //     /*
+  //      * Check if the service worker has already cached this city's weather
+  //      * data. If the service worker has the data, then display the cached
+  //      * data while the app fetches the latest data.
+  //      */
+  //     caches.match(url).then(function(response) {
+  //       if (response) {
+  //         response.json().then(function updateFromCache(json) {
+  //           var results = json.query.results;
+  //           results.key = key;
+  //           results.label = label;
+  //           results.created = json.query.created;
+  //           app.updateForecastCard(results);
+  //         });
+  //       }
+  //     });
+  //   }
+  //   // Fetch the latest data.
+  //   var request = new XMLHttpRequest();
+  //   request.onreadystatechange = function() {
+  //     if (request.readyState === XMLHttpRequest.DONE) {
+  //       if (request.status === 200) {
+  //         var response = JSON.parse(request.response);
+  //         var results = response.query.results;
+  //         results.key = key;
+  //         results.label = label;
+  //         results.created = response.query.created;
+  //         app.updateForecastCard(results);
+  //       }
+  //     } else {
+  //       // Return the initial weather forecast since no data is available.
+  //       app.updateForecastCard(initialWeatherForecast);
+  //     }
+  //   };
+  //   request.open('GET', url);
+  //   request.send();
+  // };
+
+  // // Iterate all of the cards and attempt to get the latest forecast data
+  // app.updateForecasts = function() {
+  //   var keys = Object.keys(app.visibleCards);
+  //   keys.forEach(function(key) {
+  //     app.getForecast(key);
+  //   });
+  // };
+
+  // // TODO add saveSelectedCities function here
+  // // Save list of cities to localStorage.
+  // app.saveSelectedCities = function() {
+  //   var selectedCities = JSON.stringify(app.selectedCities);
+  //   localStorage.selectedCities = selectedCities;
+  // };
+
+  /*
+   * Fake weather data that is presented when the user first uses the app,
+   * or when the user has not saved any cities. See startup code for more
+   * discussion.
+   */
+  var initialWeatherForecast = {
+    key: '2459115',
+    label: 'New York, NY',
+    created: '2016-07-22T01:00:00Z',
+    channel: {
+      astronomy: {
+        sunrise: "5:43 am",
+        sunset: "8:21 pm"
+      },
+      item: {
+        condition: {
+          text: "Windy",
+          date: "Thu, 21 Jul 2016 09:00 PM EDT",
+          temp: 56,
+          code: 24
+        },
+        forecast: [
+          {code: 44, high: 86, low: 70},
+          {code: 44, high: 94, low: 73},
+          {code: 4, high: 95, low: 78},
+          {code: 24, high: 75, low: 89},
+          {code: 24, high: 89, low: 77},
+          {code: 44, high: 92, low: 79},
+          {code: 44, high: 89, low: 77}
+        ]
+      },
+      atmosphere: {
+        humidity: 56
+      },
+      wind: {
+        speed: 25,
+        direction: 195
+      }
+    }
+  };
+  // TODO uncomment line below to test app with fake data
+  // app.updateForecastCard(initialWeatherForecast);
+
+  /************************************************************************
+   *
+   * Code required to start the app
+   *
+   * NOTE: To simplify this codelab, we've used localStorage.
+   *   localStorage is a synchronous API and has serious performance
+   *   implications. It should not be used in production applications!
+   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+   ************************************************************************/
+
+  // TODO add startup code here
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      // app.getForecast(city.key, city.label);
+    });
+  } else {
+    /* The user is using the app for the first time, or the user has not
+     * saved any cities, so show the user some fake data. A real app in this
+     * scenario could guess the user's location via IP lookup and then inject
+     * that data into the page.
+     */
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
+
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
---
+++ service-worker.js
@@ -0,0 +1,77 @@
+var dataCacheName = 'pwa-data13' + Math.random();
+var cacheName = 'pwa-data13' + Math.random();
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/material.js',
+  '/styles/inline.css',
+  '/styles/material.css',
+  '/images/logo.png',
+  '/styles/font.woff2'
+];
+
+
+/*
+ * Add specific files to cache
+ */
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://swapi.co/api';
+  // verifica se a url é a da api
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    e.respondWith(
+      // abro o cache local
+      caches.open(dataCacheName).then(function(cache) {
+        // bato na api efetivamente
+        return fetch(e.request).then(function(response){
+          // coloco a resposta no cache
+          cache.put(e.request.url, response.clone());
+          console.log(cache)
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
